5c3c04438ddf7eb922cd5ba94b2b903933764e8a
longsobad <nguyenlongdz2004@gmail.com>
2025-10-01 15:36:23 +0700

    exercise 1: functional testing

---
+++ functional-testing/wizard_rank.py
@@ -0,0 +1,28 @@
+def get_wizard_title(mana: int, attack: int) -> str:
+
+    if 10 <= mana < 50:
+        mana_level = "thap"
+    elif 50 <= mana <= 79:
+        mana_level = "trung"
+    else:
+        mana_level = "cao"
+   
+
+    if 1 <= attack <= 4:
+        attack_level = "yeu"
+    elif 5 <= attack <= 8:
+        attack_level = "trung"
+    else:
+        attack_level = "manh"
+   
+
+    if mana_level == "trung" and attack_level == "trung":
+        return "Phap su Tap su"
+    elif mana_level == "trung" and attack_level == "manh":
+        return "Phap su Chop nhoang"
+    elif mana_level == "cao" and attack_level == "trung":
+        return "Phap su Tri tue"
+    elif mana_level == "cao" and attack_level == "manh":
+        return "Phap su Toan nang"
+    else:
+        return "Nguoi thuong"
